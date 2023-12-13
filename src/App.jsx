23ac211006c9238c9bfb022ce5e6776282d18f37
vijay <vijaykv2228@gmail.com>
2023-12-13 21:17:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+  return (
+    <>
+     <div className="text-6xl text-red-700 bg-orange-400 ml-96 pt-10 ">
+      Hi Neha
+     </div>
+     
+    </>
+  )
+}
+
+export default App
